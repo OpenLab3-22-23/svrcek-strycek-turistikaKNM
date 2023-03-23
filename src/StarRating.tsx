45b7bfbed215613af1c8e984ca5ad089e8e9bdda
z20ist17 <z20ist17@z00p3-07.SPSKNM>
2023-03-23 09:09:17 +0100

    nič

--- src/StarRating.tsx
+++ src/StarRating.tsx
@@ -3,47 +3,45 @@ import "./Skuska.css";
 import Rating from '@mui/material/Rating';
 import Box from '@mui/material/Box';
 import { supabase } from "./supabase/supabaseClient";
+import { useEffect, useState } from "react";
 
 export default function StarRating(props) {
-    const [value, setValue] = React.useState(null);
+  const [value, setValue] = useState(null);
 
-    async function UpdateRating()
-    {
-    const { data } = await supabase
-  .from('profiles')
-  .update({ SKIrating: value })
-  .eq('id', 1) //session.user.id
-    }
-
-    return (
-      <div>
-        <Box align="left"  component="fieldset" mb={3} borderColor="transparent">
-        <p className="Hodnotenie">Zadaj hodnotenie:</p>
-          <Rating
-            value={value}
-            name="rating"
-            onChange={(event, newValue) => {
-              setValue(newValue);
-              console.log(newValue);
-
-            }}
-            onClick={props.handleInputChange
-     //         () => UpdateRating()
-            }
-            
-          />
-        </Box>
-               <div>
-                    MENO <span></span> <Rating name="read-only" value={value} readOnly />
-
-              </div>
-      </div>
-      
-      
-    )
-    
-    /*const { data , error } = await supabase
-     .from('profiles')
-     .update('description')*/
-     
+  async function UpdateRating() {
+    console.log("test")
+    console.log(value)
+    const { error } = await supabase.from('ski_Ratingy').update({ stars: value }).eq('location_id', 1) //session.user.id
   }
+  return (
+    <div>
+      <Box align="left" component="fieldset" mb={3} borderColor="transparent">
+        <p className="Hodnotenie">Zadaj hodnotenie:</p>
+        <Rating
+          value={value}
+          name="rating"
+          onChange={(event, newValue) => {
+            setValue(newValue);
+            //console.log(newValue)
+
+          }}
+          onClick={props.handleInputChange}
+
+
+        />
+        <button onClick={ () => UpdateRating()}> </button>
+      </Box>
+      <div>
+        MENO <span></span> <Rating name="read-only" value={value} readOnly />
+
+      </div>
+    </div>
+
+
+  )
+
+  /*const { data , error } = await supabase
+   .from('profiles')
+   .update('description')*/
+
+}
