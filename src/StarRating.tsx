3dec2f7426392141aa17c43dc9a5cc83e7aa39f7
z20ist17 <z20ist17@z00p3-07.SPSKNM>
2023-04-13 09:12:35 +0200

    nedokoncene hodnotenie

--- src/StarRating.tsx
+++ src/StarRating.tsx
@@ -5,13 +5,14 @@ import Box from '@mui/material/Box';
 import { supabase } from "./supabase/supabaseClient";
 import { useEffect, useState } from "react";
 
-export default function StarRating(props) {
+export default function StarRating() {
+  let afterValue;
   const [value, setValue] = useState(null);
 
   async function UpdateRating() {
-    console.log("test")
-    console.log(value)
-    const { error } = await supabase.from('ski_Ratingy').update({ stars: value }).eq('location_id', 1) //session.user.id
+    console.log(value);
+    const {error} = await supabase.from('ski_Ratingy').update({ stars: value }).eq('location_id', 1).select() //session.user.id
+    
   }
   return (
     <div>
@@ -21,27 +22,18 @@ export default function StarRating(props) {
           value={value}
           name="rating"
           onChange={(event, newValue) => {
+
             setValue(newValue);
-            //console.log(newValue)
 
           }}
-          onClick={props.handleInputChange}
-
 
         />
-        <button onClick={ () => UpdateRating()}> </button>
+       <button onClick={UpdateRating}  > </button>
       </Box>
       <div>
         MENO <span></span> <Rating name="read-only" value={value} readOnly />
 
       </div>
     </div>
-
-
   )
-
-  /*const { data , error } = await supabase
-   .from('profiles')
-   .update('description')*/
-
 }
